feed08c81d1a9588b1473fb5284a178b8a5127a8
Mariusz Borek <mariuszborek242@gmail.com>
2020-10-12 01:45:08 +0200

    preapred tameplate for notes

---
+++ src/app/note.ts
@@ -0,0 +1,5 @@
+export interface Note {
+  id: number;
+  topic: string;
+  text: string;
+}
--- src/app/notes/notes.component.ts
+++ src/app/notes/notes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Note } from '../note';
 
 @Component({
   selector: 'app-notes',
@@ -6,6 +7,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./notes.component.scss']
 })
 export class NotesComponent implements OnInit {
+
+  note1: Note = {
+    id: 1,
+    topic: 'Hamownia',
+    text: 'sprawdź drzwi miedzy hamownia a halą E'
+  };
+
+  note2: Note = {
+    id: 2,
+    topic: 'Dach bud B',
+    text: 'sprawdź drzwi miedzy ...'
+  };
+
+  note3: Note = {
+    id: 3,
+    topic: 'Sufity',
+    text: 'sprawdź drzwi miedzy hamownia a halą E'
+  };
+
+  notes: Note[] = [this.note1, this.note2, this.note3];
 
   constructor() { }
 
